f749d0010a314414b73e1973206070b2aeecb12b
Lyz03 <lizoe@netcourrier.com>
2021-11-29 13:45:48 +0100

    html & js

---
+++ app.js
@@ -0,0 +1,54 @@
+const userName = document.querySelector("input[type='text']");
+
+let name = [
+    "Erwin",
+    "Eren",
+    "Livai",
+    "Mikasa",
+    "Armin"
+];
+
+let objects = [
+    " une Eppé",
+    " un Equipement Tridimentionelle",
+    " un Balais",
+    " une Hache"
+];
+
+let temperatures = [
+    "10°",
+    "20°",
+    "15°",
+    "5°",
+    "0°"
+];
+
+let places = [
+    " à la Zone d'entrainement ",
+    " au District de Trost ",
+    " au District de Shiganshina ",
+    " à l'Exterieur des murs ",
+    " au District de Stohese "
+];
+
+let verbs = [
+    "se Battre contre les Titans",
+    "s'entrainer",
+    "se Battre contre le Cuirasse",
+    "se Battre contre le Colosal",
+    "se Battre contre le Bestial"
+];
+
+/*
+* return : the name of your array[random number]
+ */
+function randomIndex(arrayName) {
+    const randomNumber = Math.floor(Math.random() * arrayName.length);
+    return arrayName[randomNumber];
+}
+
+document.querySelector('button').addEventListener("click", function () {
+    document.getElementById('story').innerText += userName.value + " et " + randomIndex(name) + " sont allés "
+    + randomIndex(places) + " pour " + randomIndex(verbs) + " avec " + randomIndex(objects) + " alors qu'il faisaient "
+    + randomIndex(temperatures) + "." + '\n';
+})
